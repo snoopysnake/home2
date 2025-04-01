4cbdd495c2a235b751c3d110d33380dce1b374ae
snoopysnake <chiang4@gmail.com>
2025-04-01 14:11:14 -0400

    add katex

--- app/maths/math-stuff.tsx
+++ app/maths/math-stuff.tsx
@@ -1,9 +1,24 @@
+import { useState, useEffect, useRef } from 'react';
+import katex from 'katex';
 import './maths.css';
 
 export function MathStuff() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    if (containerRef.current) {
+      katex.render("c = \\pm\\sqrt{a^2 + b^2}", containerRef.current, {
+        throwOnError: false
+      });
+    }
+
+  }, []);
+
+
   return (
     <main>
       <h1>alex chiang math stuff.</h1>
+      <div ref={containerRef}></div>
     </main>
   );
 }
